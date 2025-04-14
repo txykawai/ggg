b39562ecaf27c79fc6494479ac91ca60cb7bbe5a
sixteen <txy5601571@outlook.com>
2025-04-14 10:39:59 +0800

    message 11: Show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item
 
+
 class ItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
@@ -26,6 +27,14 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
+	def test_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/')
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 1', response.content.decode())
+		
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')          
 		self.assertEqual(found.func, home_page)   
@@ -36,5 +45,17 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+
+	def test_only_saves_items_when_necessary(self):
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(), 0)
