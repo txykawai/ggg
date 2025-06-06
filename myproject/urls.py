86a5cd1df3db2a10a51efb485f462a2c777a67b2
sixteen <txy5601571@outlook.com>
2025-06-06 16:01:43 +0800

    message 16: form URL of new list by POST

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.urls import path
 from lists import views
 
 urlpatterns = [
-	path('', views.home_page, name = 'home'),
-    path('lists/the-new-page/', views.view_list, name = 'view_list')
+    path('', views.home_page, name='home'),
+    path('lists/the-new-page/', views.view_list, name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
 ]
