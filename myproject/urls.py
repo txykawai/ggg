177daf7a3fa156438b7852e33a3e464487d2fefe
sixteen <txy5601571@outlook.com>
2025-06-06 17:31:37 +0800

    message19: finish 14 chapter

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,25 +1,8 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import path, re_path
-from lists import views
+from django.urls import path, re_path, include
+from lists import views as list_views  # (1)
+from lists import urls as list_urls   # (1)
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    path('lists/new', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    path('', list_views.home_page, name='home'),
+    path('lists/', include(list_urls)),  # (2)
 ]
